9fc10227bf0dad740ae75e09c15ab838b9ca0e51
Juan Meaño <juanmeanoc@gmail.com>
2018-06-08 09:08:15 -0430

    Render Calendar and days in same cycle

--- main.js
+++ main.js
@@ -9,10 +9,6 @@ const datepicker = new Vue({
         headerMonth: [],
         headerYear: [],
         daysPrint: [],
-        daysPerMonth: [],
-        header : [{
-            month:[], year:[], days:[]
-        }],
         months:
 ['January','February','March','April','May','June','July','August','September','October','November','December'],
 
@@ -24,7 +20,8 @@ const datepicker = new Vue({
 
             this.headerMonth = [];
             this.daysPrint = [];
-            this.daysPerMonth= [];
+
+
 
             var yearInit = this.date.getFullYear();
             var monthInit = this.date.getMonth()+1;
@@ -34,42 +31,22 @@ const datepicker = new Vue({
             //alert(end_date);
 
             numberMonths = moment(end_date).diff(moment(fecha),'months')+1; //numero de meses
-            var a;
+
             for(var k = 0, j = monthInit - 1; k < numberMonths; k++, j++){
 
-                //this.headerMonth.push(this.months[j]+'-'+yearInit);
+                this.headerMonth.push(this.months[j]+'-'+yearInit);
 
                 var dynamicDate = new Date(yearInit, j, 1)
-                //var month = dynamicDate.getMonth()+1;
+
                 //alert(dynamicDate)
-                a = renderCalendar(dynamicDate);
-
-        
-
-                this.header.push({ month: this.months[j], year: yearInit, days: a});
-
-                
-                //alert(a);
-                //this.daysPrint = a;
-
-                if(this.months[j] == 'December'){
-                        j = -1;
-                        yearInit++;
-                    }
-            }
-            //alert(month);
-
-            function renderCalendar(date) {
-                //alert(date)
-                //var firstDay = date.getDate();
-                //alert(firstDay)
-                var month = date.getMonth()+1;
-                var year = date.getFullYear();
-                var now = new Date(year,month-1,1); //1er dia del mes actual seleccionado fecha larga
-                var last = new Date(year,month,0); //ultimo dia del mes seleccionado fecha larga
-                var firstDayWeek=date.getDay(); //primer dia nominal seleccionado
-                var lastDayName = new Date(last).getDay(); //Ultimo dia del mes Nominal BORRAR
-                var lastDayMonth=last.getDate(); //ultimo dia del mes seleccionado DD
+                ////////////////
+                var month = dynamicDate.getMonth()+1;
+                var year = dynamicDate.getFullYear();
+                var now = new Date(year,month-1,1); //1er dia del mesactual seleccionado fecha larga
+                var last = new Date(year,month,0); //ultimo dia delmes seleccionado fecha larga
+                var firstDayWeek=dynamicDate.getDay(); //primer dia nominalseleccionado
+                var lastDayName = new Date(last).getDay(); //Ultimodia del mes Nominal BORRAR
+                var lastDayMonth=last.getDate(); //ultimo dia del messeleccionado DD
                 var last_cell=firstDayWeek+lastDayMonth; //total de celdas
                 var days = [];
 
@@ -84,18 +61,36 @@ const datepicker = new Vue({
                     if(i == firstDayWeek) //captura el primer dia
                     {
                         dia = 1;
-                        days.push(dia);
+                        this.daysPrint.push(dia+'-'+month);
                     }
                     if(i < firstDayWeek || i >= last_cell - 1){
                       //  alert(this.lastDayMonth);
-                        if(i%7 == 6 && dia == lastDayMonth)
-                            break;
-                        days.push('x'); //celda vacia
+                        if(i%7 == 6 && dia == lastDayMonth){
+                            //break;
+                        //this.daysPrint.push('-----');
+                        break; } //celda vacia
+                        this.daysPrint.push('x'); //celda vacia
                         }
                     else
-                        days.push(++dia);
+                        this.daysPrint.push(++dia+'-'+month);
 
                 }
+                ////////////////
+
+              
+
+                if(this.months[j] == 'December'){
+                        j = -1;
+                        yearInit++;
+                    }
+            }
+            //alert(month);
+
+            function renderCalendar(date) {
+                //alert(date)
+                //var firstDay = date.getDate();
+                //alert(firstDay)
+               
 
                 return days;
 
