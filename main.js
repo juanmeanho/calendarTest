e17fa326653f917bcf0b201bfb4b7ed27a6340da
Juan Meaño <juanmeanoc@gmail.com>
2018-06-08 08:37:49 -0430

    Dynamic render of the days

--- main.js
+++ main.js
@@ -9,6 +9,10 @@ const datepicker = new Vue({
         headerMonth: [],
         headerYear: [],
         daysPrint: [],
+        daysPerMonth: [],
+        header : [{
+            month:[], year:[], days:[]
+        }],
         months:
 ['January','February','March','April','May','June','July','August','September','October','November','December'],
 
@@ -20,6 +24,7 @@ const datepicker = new Vue({
 
             this.headerMonth = [];
             this.daysPrint = [];
+            this.daysPerMonth= [];
 
             var yearInit = this.date.getFullYear();
             var monthInit = this.date.getMonth()+1;
@@ -29,18 +34,23 @@ const datepicker = new Vue({
             //alert(end_date);
 
             numberMonths = moment(end_date).diff(moment(fecha),'months')+1; //numero de meses
-
+            var a;
             for(var k = 0, j = monthInit - 1; k < numberMonths; k++, j++){
 
-                this.headerMonth.push(this.months[j]+'-'+yearInit);
+                //this.headerMonth.push(this.months[j]+'-'+yearInit);
 
                 var dynamicDate = new Date(yearInit, j, 1)
-
+                //var month = dynamicDate.getMonth()+1;
                 //alert(dynamicDate)
+                a = renderCalendar(dynamicDate);
 
-                var a = renderCalendar(dynamicDate);
+        
+
+                this.header.push({ month: this.months[j], year: yearInit, days: a});
+
+                
                 //alert(a);
-                this.daysPrint = a;
+                //this.daysPrint = a;
 
                 if(this.months[j] == 'December'){
                         j = -1;
@@ -56,10 +66,10 @@ const datepicker = new Vue({
                 var month = date.getMonth()+1;
                 var year = date.getFullYear();
                 var now = new Date(year,month-1,1); //1er dia del mes actual seleccionado fecha larga
-                var last = new Date(year,month,0); //ultimo dia delmes seleccionado fecha larga
+                var last = new Date(year,month,0); //ultimo dia del mes seleccionado fecha larga
                 var firstDayWeek=date.getDay(); //primer dia nominal seleccionado
-                var lastDayName = new Date(last).getDay(); //Ultimodia del mes Nominal BORRAR
-                var lastDayMonth=last.getDate(); //ultimo dia del messeleccionado DD
+                var lastDayName = new Date(last).getDay(); //Ultimo dia del mes Nominal BORRAR
+                var lastDayMonth=last.getDate(); //ultimo dia del mes seleccionado DD
                 var last_cell=firstDayWeek+lastDayMonth; //total de celdas
                 var days = [];
 
@@ -67,7 +77,7 @@ const datepicker = new Vue({
                 //this.headerYear = year;
 
                 var dia=0;
-                //alert(now+'-'+last+'-'+firstDayWeek+'-'+lastDayMonth+'-'+last_cell)
+//alert(now+'-'+last+'-'+firstDayWeek+'-'+lastDayMonth+'-'+last_cell)
 
                 for(var i = 0; i <= 41 ; i++) //numero maximo de celdas
                 {
